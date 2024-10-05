eae77517d528826a5bb03f7fe666417f85789b18
n-yam <n.yamamoto.py@gmail.com>
2024-10-05 16:14:18 +0900

    [fix] Props: index.propertiesを読み込めない問題を修正

--- src/main/java/index/Props.java
+++ src/main/java/index/Props.java
@@ -1,15 +1,15 @@
 package index;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 final class Props {
 	private static Properties props = new Properties();
+	private static ClassLoader classLoader= new ClassLoader(){};
 
 	static {
 		try {
-			props.load(new FileInputStream("index.properties"));
+			props.load(classLoader.getResourceAsStream("index.properties"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
